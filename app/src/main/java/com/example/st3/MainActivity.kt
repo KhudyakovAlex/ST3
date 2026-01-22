1fc67016a6595c1438f8bcae68ffdcbe57e1cce2
KhudyakovAlex <580alx@gmail.com>
2026-01-22 17:54:48 +0300

    UIAIMain and UIAIChat

--- app/src/main/java/com/example/st3/MainActivity.kt
+++ app/src/main/java/com/example/st3/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,29 +17,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ST3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MainContent() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        UIButtons(modifier = Modifier.fillMaxSize())
+        UIAI(modifier = Modifier.fillMaxSize())
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun MainPreview() {
     ST3Theme {
-        Greeting("Android")
+        MainContent()
     }
 }
---
+++ app/src/main/java/com/example/st3/UIAI.kt
@@ -0,0 +1,14 @@
+package com.example.st3
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun UIAI(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        UIAIChat(modifier = Modifier.fillMaxSize())
+        UIAIMain(modifier = Modifier.fillMaxSize())
+    }
+}
