5910af29704ff7af4c78ab402bf01427d223a6b2
KhudyakovAlex <580alx@gmail.com>
2026-01-28 08:51:24 +0300

    TOKENS RESTRUCTURED: 217 tokens (was 281), removed typography aliases, added new radius/stroke tokens

--- app/src/main/java/com/example/st3/ui/theme/PixsoDimens.kt
+++ app/src/main/java/com/example/st3/ui/theme/PixsoDimens.kt
@@ -1,4 +1,4 @@
-package com.example.st3.ui.theme
+﻿package com.example.st3.ui.theme
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -6,7 +6,6 @@ import androidx.compose.ui.unit.sp
 object PixsoDimens {
 
     // ===== BASE VALUES =====
-    // Border
     val Border_width_border_width_1 = 1.dp
     val Border_width_border_width_12 = 12.dp
     val Border_width_border_width_16 = 16.dp
@@ -14,8 +13,6 @@ object PixsoDimens {
     val Border_width_border_width_24 = 24.dp
     val Border_width_border_width_4 = 4.dp
     val Border_width_border_width_8 = 8.dp
-
-    // Font
     val Font_Line_Height_Line_Height_16 = 16.sp
     val Font_Line_Height_Line_Height_20 = 20.sp
     val Font_Line_Height_Line_Height_24 = 24.sp
@@ -41,113 +38,62 @@ object PixsoDimens {
     val Font_Size_Size_48 = 48.sp
     val Font_Size_Size_52 = 52.sp
     val Font_Size_Size_56 = 56.sp
-    val Font_Tracking_Tracking_0 = 0.sp
-    val Font_Tracking_Tracking_1 = 0.5.sp
-    val Font_Tracking_Tracking_2 = (-0.3).sp
-    val Font_Tracking_Tracking_3 = 0.3.sp
-    val Font_Tracking_Tracking_4 = 0.4.sp
-    val Font_Tracking_Tracking_5 = 0.1.sp
-    val Font_Tracking_Tracking_6 = 0.2.sp
-
-    // Radius
-    val Radius_Radius_12 = 12.dp
-    val Radius_Radius_16 = 16.dp
-    val Radius_Radius_20 = 20.dp
-    val Radius_Radius_24 = 24.dp
-    val Radius_Radius_28 = 28.dp
-    val Radius_Radius_32 = 32.dp
-    val Radius_Radius_36 = 36.dp
-    val Radius_Radius_4 = 4.dp
-    val Radius_Radius_40 = 40.dp
-    val Radius_Radius_8 = 8.dp
-    val Radius_Radius_Full = 200.dp
-    val Radius_Radius_None = 0.dp
-
-    // Space
-    val Space_Space_0 = 0.dp
-    val Space_Space_12 = 12.dp
-    val Space_Space_16 = 16.dp
-    val Space_Space_20 = 20.dp
-    val Space_Space_24 = 24.dp
-    val Space_Space_28 = 28.dp
-    val Space_Space_32 = 32.dp
-    val Space_Space_36 = 36.dp
-    val Space_Space_4 = 4.dp
-    val Space_Space_40 = 40.dp
-    val Space_Space_48 = 48.dp
-    val Space_Space_52 = 52.dp
-    val Space_Space_56 = 56.dp
-    val Space_Space_8 = 8.dp
-    val Space_Space_88 = 88.dp
+    val Numeric_0 = 0.dp
+    val Numeric_1 = 1.dp
+    val Numeric_12 = 12.dp
+    val Numeric_16 = 16.dp
+    val Numeric_20 = 20.dp
+    val Numeric_200 = 200.dp
+    val Numeric_24 = 24.dp
+    val Numeric_28 = 28.dp
+    val Numeric_32 = 32.dp
+    val Numeric_36 = 36.dp
+    val Numeric_4 = 4.dp
+    val Numeric_40 = 40.dp
+    val Numeric_48 = 48.dp
+    val Numeric_52 = 52.dp
+    val Numeric_56 = 56.dp
+    val Numeric_8 = 8.dp
+    val Numeric_88 = 88.dp
 
     // ===== ALIASES =====
-    // Body
     val Body_Body_L_Line_Height = Font_Line_Height_Line_Height_24
     val Body_Body_L_Size = Font_Size_Size_16
-    val Body_Body_L_Tracking = Font_Tracking_Tracking_1
     val Body_Body_M_Line_Height = Font_Line_Height_Line_Height_20
     val Body_Body_M_Size = Font_Size_Size_14
-    val Body_Body_M_Tracking = Font_Tracking_Tracking_3
     val Body_Body_S_Line_Height = Font_Line_Height_Line_Height_16
     val Body_Body_S_Size = Font_Size_Size_12
-    val Body_Body_S_Tracking = Font_Tracking_Tracking_4
-
-    // Button
     val Button_Button_L_Line_Height = Font_Line_Height_Line_Height_20
     val Button_Button_L_Size = Font_Size_Size_20
-    val Button_Button_L_Tracking = Font_Tracking_Tracking_0
     val Button_Button_M_Line_Height = Font_Line_Height_Line_Height_20
     val Button_Button_M_Size = Font_Size_Size_16
-    val Button_Button_M_Tracking = Font_Tracking_Tracking_1
     val Button_Button_S_Line_Height = Font_Line_Height_Line_Height_16
     val Button_Button_S_Size = Font_Size_Size_14
-    val Button_Button_S_Tracking = Font_Tracking_Tracking_1
-
-    // Display
     val Display_Display_L_Line_Height = Font_Line_Height_Line_Height_64
     val Display_Display_L_Size = Font_Size_Size_56
-    val Display_Display_L_Tracking = Font_Tracking_Tracking_2
     val Display_Display_M_Line_Height = Font_Line_Height_Line_Height_52
     val Display_Display_M_Size = Font_Size_Size_44
-    val Display_Display_M_Tracking = Font_Tracking_Tracking_0
     val Display_Display_S_Line_Height = Font_Line_Height_Line_Height_44
     val Display_Display_S_Size = Font_Size_Size_36
-    val Display_Display_S_Tracking = Font_Tracking_Tracking_0
-
-    // Headline
     val Headline_Headline_L_Line_Height = Font_Line_Height_Line_Height_40
     val Headline_Headline_L_Size = Font_Size_Size_32
-    val Headline_Headline_L_Tracking = Font_Tracking_Tracking_0
     val Headline_Headline_M_Line_Height = Font_Line_Height_Line_Height_36
     val Headline_Headline_M_Size = Font_Size_Size_28
-    val Headline_Headline_M_Tracking = Font_Tracking_Tracking_0
     val Headline_Headline_S_Line_Height = Font_Line_Height_Line_Height_32
     val Headline_Headline_S_Size = Font_Size_Size_24
-    val Headline_Headline_S_Tracking = Font_Tracking_Tracking_0
     val Headline_Headline_XS_Line_Height = Font_Line_Height_Line_Height_32
     val Headline_Headline_XS_Size = Font_Size_Size_20
-    val Headline_Headline_XS_Tracking = Font_Tracking_Tracking_0
-
-    // Label
     val Label_Label_L_Line_Height = Font_Line_Height_Line_Height_20
     val Label_Label_L_Size = Font_Size_Size_14
-    val Label_Label_L_Tracking = Font_Tracking_Tracking_5
     val Label_Label_M_Line_Height = Font_Line_Height_Line_Height_16
     val Label_Label_M_Size = Font_Size_Size_12
-    val Label_Label_M_Tracking = Font_Tracking_Tracking_1
     val Label_Label_S_Line_Height = Font_Line_Height_Line_Height_16
     val Label_Label_S_Size = Font_Size_Size_11
-    val Label_Label_S_Tracking = Font_Tracking_Tracking_5
-
-    // Title
-    val Title_Title_L_Line_Height = Font_Line_Height_Line_Height_28
-    val Title_Title_L_Size = Font_Size_Size_24
-    val Title_Title_L_Tracking = Font_Tracking_Tracking_0
-    val Title_Title_M_Line_Height = Font_Line_Height_Line_Height_24
-    val Title_Title_M_Size = Font_Size_Size_16
-    val Title_Title_M_Tracking = Font_Tracking_Tracking_6
-    val Title_Title_S_Line_Height = Font_Line_Height_Line_Height_20
-    val Title_Title_S_Size = Font_Size_Size_14
-    val Title_Title_S_Tracking = Font_Tracking_Tracking_5
-
+    val Radius_Radius_Full = Numeric_200
+    val Radius_Radius_L = Numeric_40
+    val Radius_Radius_M = Numeric_24
+    val Radius_Radius_None = Numeric_0
+    val Radius_Radius_S = Numeric_16
+    val Stroke_L = Numeric_12
+    val Stroke_S = Numeric_1
 }
